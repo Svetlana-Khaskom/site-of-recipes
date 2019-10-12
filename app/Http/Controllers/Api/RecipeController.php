63ee8a11862bd81deab3133b1a87b7274a72e740
Anton Bodnar <bodnar.nixsolutions@gmail.com>
2019-10-12 11:53:09 +0300

    create repositories

--- app/Http/Controllers/Api/RecipeController.php
+++ app/Http/Controllers/Api/RecipeController.php
@@ -5,12 +5,21 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\RecipeResource;
+use App\Repositories\RecipeRepository;
 
 class RecipeController extends Controller
 {
+    private $recipeRepository;
+
+    public function __construct(RecipeRepository $recipeRepository)
+    {
+        $this->recipeRepository = $recipeRepository;
+    }
+
     public function getRecipesByUserId($userId)
     {
-        $recipes = User::find($userId)->recipes()->get();
+        $recipes = $this->recipeRepository->getByUser(User::find($userId));
+
         return RecipeResource::collection($recipes);
     }
 }
--- app/Repositories/RecipeRepository.php
+++ app/Repositories/RecipeRepository.php
@@ -1,10 +1,16 @@
 <?php
 
-
 namespace App\Repositories;
-
 
 class RecipeRepository
 {
-
+    /**
+     * @param $user
+     *
+     * @return mixed
+     */
+    public function getByUser($user)
+    {
+        return $user->recipes()->get();
+    }
 }
